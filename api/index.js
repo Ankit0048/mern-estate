3ec7aada0f114702a048e0414c80ee46cff8b753
Ankit <ankit20aug.kumar@gmail.com>
2023-10-21 15:49:48 +0530

    Added the authentication and error backend handling

--- api/index.js
+++ api/index.js
@@ -1,10 +1,15 @@
 import express from 'express';
 import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
-import userRouter from './routes/user.route.js'
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
+
 
 dotenv.config();
 const app = express();
+
+app.use(express.json());
+
 mongoose.connect(process.env.MONGO).then(
     () => {
         console.log("Connected to DB!");
@@ -20,3 +25,14 @@ app.listen(3000, () => {
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
+
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
+    return res.status(statusCode).json({
+        success: false,
+        statusCode,
+        message,
+    });
+});
