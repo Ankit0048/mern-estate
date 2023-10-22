93abe4d2f040ffac3e950fe85044809d57cf4985
Ankit <ankit20aug.kumar@gmail.com>
2023-10-22 14:27:07 +0530

    Create user update api

--- api/contorllers/user.controller.js
+++ api/contorllers/user.controller.js
@@ -1,5 +1,34 @@
+import bcryptjs from 'bcryptjs'
+import User from '../models/user.model.js';
+
 export const test = (req, res) => {
     res.json({
         message: "Create a new function full route user",
     });
 };
+
+export const updateUser =async (req, res, next) => {
+    if (req.user.id !== req.params.id) return next(errorHandler(401, "You can only update your own account"));
+    try {
+        if (req.body.password) {
+            req.body.password = bcryptjs.hashSync(req.body.password, 10);
+        }
+
+        const updateddUser = await User.findByIdAndUpdate(req.params.id, {
+           $set: {
+            username: req.body.username,
+            email: req.body.email,
+            passowrd: req.body.password,
+            avatar: req.body.avatar,
+           },
+        },
+        {new: true});
+
+        const {password, ...rest} = updateddUser._doc;
+        res.status(200).json(rest)
+        res.success = true;
+    }
+    catch(err) {
+        next(err);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -3,12 +3,12 @@ import mongoose, { mongo } from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
-
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-
+app.use(cookieParser());
 mongoose.connect(process.env.MONGO).then(
     () => {
         console.log("Connected to DB!");
